12c772b44e1f14eecd40752f8519df0b31fc1f84
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-09-22 08:07:28 -0300

    feat(DIZ-18): Created the color extension system

---
+++ Dizcarta/Core/DesignSystems/Extensions/UIColor+Color.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Color.swift
+//  Dizcarta
+//
+//  Created by Felipe Brigagão de Almeida on 22/09/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  public static let avatarColorPurple: Color = Color(UIColor(named: "avatarColorPurple") ?? .black)
+  public static let avatarColorGreen: Color = Color(UIColor(named: "avatarColorGreen") ?? .black)
+  public static let avatarColorCopperRose: Color = Color(UIColor(named: "avatarColorCopperRose") ?? .black)
+  public static let avatarColorYellow: Color = Color(UIColor(named: "avatarColorYellow") ?? .black)
+  public static let avatarColorBlue: Color = Color(UIColor(named: "avatarColorBlue") ?? .black)
+  public static let avatarColorPatina: Color = Color(UIColor(named: "avatarColorPatina") ?? .black)
+
+}
