5ae3138eece3696884421e4b8967803cf5b05720
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-09-26 15:30:55 -0300

    feat(DIZ-88): Created the History screen

---
+++ Dizcarta/Screens/History/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  Dizcarta
+//
+//  Created by Felipe Brigagão de Almeida on 26/09/22.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
