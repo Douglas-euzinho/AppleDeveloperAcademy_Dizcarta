017944440ab3237a79fef5123d22c85ddae4117e
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2022-09-28 17:12:53 -0300

    fear(DIZ-96): created shift player view ("tela de turno")

---
+++ Dizcarta/Screens/ShiftPlayer/ShiftPlayerView.swift
@@ -0,0 +1,36 @@
+//
+//  ShiftPlayerView.swift
+//  Dizcarta
+//
+//  Created by Victor Brito on 28/09/22.
+//
+
+import SwiftUI
+
+struct ShiftPlayerView: View {
+    var body: some View {
+        ZStack {
+            Color.avatarColorYellow
+                .ignoresSafeArea(.all)
+                
+            VStack {
+                VStack {
+                    Text("Agora é a vez de:")
+                        .padding(15)
+                    Text("Nome do player")
+                        .font(.largeTitle)
+                }
+                .padding(.bottom, 25)
+                GenericButtons(label: "Embaralhar")
+                    .frame(width: 251, height: 46)
+                    .shadow(radius: 10)
+            }
+        }
+    }
+}
+
+struct ShiftPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShiftPlayerView()
+    }
+}
