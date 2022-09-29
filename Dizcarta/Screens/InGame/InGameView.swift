f7859511f995882f099befa2eda297f7a0e95ad1
Douglas Figueirôa <62218512+Douglas-euzinho@users.noreply.github.com>
2022-09-29 16:09:13 -0300

    feat(DIZ-118): added the close button in game flow

--- Dizcarta/Screens/InGame/InGameView.swift
+++ Dizcarta/Screens/InGame/InGameView.swift
@@ -8,47 +8,54 @@
 import SwiftUI
 
 struct InGameView: View {
-  // MARK: - VARIABLES
-  @State var backDegree = 0.0
-  @State var frontDegree = -90.0
-  @State var isFlipped = false
-  
-  let durationAndDelay : CGFloat = 0.3
-  
-  var body: some View {
-    ZStack {
-      FrontCard(image: .constant("house.fill"),
-                title: .constant("Doente de Amor"),
-                description: .constant("Você deve ficar de mãos dadas com a pessoa a sua esquerda enquanto joga."),
-                acceptPoints: .constant(4), declinePoints: .constant(4), degree: $frontDegree)
-      BackCard(degree: $backDegree)
-    }.onTapGesture {
-      flipCard()
+    // MARK: - VARIABLES
+    @State var backDegree = 0.0
+    @State var frontDegree = -90.0
+    @State var isFlipped = false
+    
+    let durationAndDelay : CGFloat = 0.3
+    
+    var body: some View {
+        ZStack {
+            GenericFunctions.checkIfImageExist(name: "ExitButton")
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: Alignment.topLeading)
+                .padding()
+            VStack {
+                ZStack {
+                    FrontCard(image: .constant("house.fill"),
+                              title: .constant("Doente de Amor"),
+                              description: .constant("Você deve ficar de mãos dadas com a pessoa a sua esquerda enquanto joga."),
+                              acceptPoints: .constant(4), declinePoints: .constant(4), degree: $frontDegree)
+                    BackCard(degree: $backDegree)
+                }
+            }.onTapGesture {
+                flipCard()
+            }
+        }
     }
-  }
     // MARK: Flip Card Function
     func flipCard() {
-      isFlipped = !isFlipped
-      if isFlipped {
-        withAnimation(.linear(duration: durationAndDelay)) {
-          backDegree = 90
+        isFlipped = !isFlipped
+        if isFlipped {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                backDegree = 90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
+                frontDegree = 0
+            }
+        } else {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                frontDegree = -90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
+                backDegree = 0
+            }
         }
-        withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
-          frontDegree = 0
-        }
-      } else {
-        withAnimation(.linear(duration: durationAndDelay)) {
-          frontDegree = -90
-        }
-        withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)) {
-          backDegree = 0
-        }
-      }
     }
 }
 
 struct InGameView_Previews: PreviewProvider {
-  static var previews: some View {
-    InGameView()
-  }
+    static var previews: some View {
+        InGameView()
+    }
 }
