694e8c735b1d8e96eeabf037c73de0409df7cab6
Igor Silva <igorsilvadeveloper@gmail.com>
2022-10-05 17:59:10 -0300

    fix(DIZ-144): remove players from core data when dismiss playerlist vie

--- Dizcarta/Screens/PlayerList/Shared/PlayerList+Observed.swift
+++ Dizcarta/Screens/PlayerList/Shared/PlayerList+Observed.swift
@@ -9,12 +9,42 @@ import Foundation
 import CoreData
 
 extension PlayerListView {
-    class Observed: ObservableObject {
-        @Published var repository: PlayerRepositoryProtocol
+    class Observed: ObservableObject, PlayerRepositoryProtocol {
+        
+        @Published var context: NSManagedObjectContext
+        @Published var matchInProgress: MatchInProgress
+        @Published var players: [Player] = []
+        private var repository: PlayerRepositoryProtocol
         
         init(context: NSManagedObjectContext) {
+            self.context = context
             self.repository = PlayerRepository.get(context: context)
+            self.matchInProgress = repository.createMatch()
         }
         
+        internal func getPlayers() -> [Player] {
+            repository.getPlayers()
+        }
+        
+        func fetchPlayers() {
+            self.players = getPlayers()
+        }
+        
+        func createPlayer(name: String, avatar: String, match: MatchInProgress) {
+            repository.createPlayer(name: name, avatar: avatar, match: match)
+        }
+        
+        internal func createMatch() -> MatchInProgress {
+            self.repository.createMatch()
+        }
+        
+        func resetMatch() {
+            let players = matchInProgress.players?.allObjects as? [Player]
+            guard let players else { return }
+            players.forEach { player in
+                repository.delete(object: player)
+            }
+            repository.delete(object: matchInProgress)
+        }
     }
 }
