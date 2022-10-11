53365f13b344d44aeb373e7c806b7214ee831db4
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2022-10-11 17:33:11 -0300

    feature: created splash screen with json animations

---
+++ Dizcarta/Screens/CardAnimation /CardsAnimation.swift
@@ -0,0 +1,20 @@
+//
+//  CardsAnimation.swift
+//  Dizcarta
+//
+//  Created by Victor Brito on 10/10/22.
+//
+
+import SwiftUI
+
+struct CardsAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardsAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsAnimation()
+    }
+}
---
+++ Dizcarta/Screens/CardAnimation /LottieView.swift
@@ -0,0 +1,8 @@
+//
+//  LottieView.swift
+//  Dizcarta
+//
+//  Created by Victor Brito on 10/10/22.
+//
+
+import Foundation
