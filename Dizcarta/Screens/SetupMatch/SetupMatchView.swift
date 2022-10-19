9beca8f0e3a923d41bf76db156f990f36b2990fe
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-10-19 15:58:06 -0300

    fix(DIZ-173): Added the message when there are no players added to the match

--- Dizcarta/Screens/SetupMatch/SetupMatchView.swift
+++ Dizcarta/Screens/SetupMatch/SetupMatchView.swift
@@ -22,41 +22,57 @@ struct SetupMatchView: View {
     
     // MARK: - BODY
     var body: some View {
-            GeometryReader { geometry in
-                ZStack {
-                    Color(.backgroundAppColor)
-                        .ignoresSafeArea()
-                    VStack {
-                        Spacer()
-                        if !gameCore.avatarDataList.isEmpty {
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                ZStack {
-                                    LazyHStack(spacing: 0) {
-                                        ForEach(gameCore.avatarDataList, id: \.id) { avatar in
-                                            Avatar(avatar: avatar.image, name: avatar.name, isSelection: true)
-                                                .environmentObject(gameCore)
-                                        } //: FOREACH
-                                    } //: LAZYHSTACK
-                                } //: ZSTACK
-                            } //: SCROLL VIEW
-                            .frame(height: 100)
-                        }
+        GeometryReader { geometry in
+            ZStack {
+                Color(.backgroundAppColor)
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    if !gameCore.avatarDataList.isEmpty {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            ZStack {
+                                LazyHStack(spacing: 0) {
+                                    ForEach(gameCore.avatarDataList, id: \.id) { avatar in
+                                        Avatar(avatar: avatar.image, name: avatar.name, isSelection: true)
+                                            .environmentObject(gameCore)
+                                    } //: FOREACH
+                                } //: LAZYHSTACK
+                            } //: ZSTACK
+                        } //: SCROLL VIEW
+                        .frame(height: 100)
+                    }
+                    
+                    if !gameCore.players.isEmpty {
                         ScrollView(.vertical, showsIndicators: false) {
                             ForEach(gameCore.players, id: \.self) { player in
                                 PlayerSelectedView(imagePlayer: player.wrappedAvatar, playerName: player.wrappedName)
                                     .frame(width: UIScreen.main.bounds.width - 5, height: 85)
-
+                                
                             }
                         }
-                        NavigationLink(destination: ShiftPlayerView()) {
-                            NeonButton(text: "Jogar", image: .neonButtonYellow)
-                                .opacity( (gameCore.players.count >= 4 && gameCore.players.count <= 6) ? 1.0 : 0.5)
-                                .frame(width: geometry.size.width/1.2, height: geometry.size.height/7)
-                                .padding()
-                        }
-                    } //: VSTACK
-                }
-            } //: ZSTACK
+                    } else {
+                        Spacer(minLength: geometry.size.height / 3.7)
+                        
+                        Text("Adicione pelo menos quatro personagens para" + "\ncomeçar a jogar.")
+                            .font(Font.custom("DINAlternate-Bold", size: 15))
+                            .lineLimit(2)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .frame(height: geometry.size.height / 10)
+                            .padding(.horizontal, 15)
+                        
+                        Spacer(minLength: geometry.size.height / 3.7)
+                    }
+                    
+                    NavigationLink(destination: ShiftPlayerView()) {
+                        NeonButton(text: "Jogar", image: .neonButtonYellow)
+                            .opacity( (gameCore.players.count >= 4 && gameCore.players.count <= 6) ? 1.0 : 0.5)
+                            .frame(width: geometry.size.width / 1.2, height: geometry.size.height / 7)
+                            .padding()
+                    }
+                } //: VSTACK
+            }
+        } //: ZSTACK
         .navigationBarTitle("Jogadores")
         .tint(.white)
         .navigationBarTitleDisplayMode(.inline)
