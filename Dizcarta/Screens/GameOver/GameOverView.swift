16e54d11016bcf9e402b31acb2e12c6cb9d7b7b5
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-10-20 15:47:43 -0300

    feat(DIZ-178): Adjusted the GameOver screen

--- Dizcarta/Screens/GameOver/GameOverView.swift
+++ Dizcarta/Screens/GameOver/GameOverView.swift
@@ -12,33 +12,49 @@ struct GameOverView: View {
   
   // MARK: - BODY
   var body: some View {
-    VStack {
+    ZStack {
+      Color(.backgroundAppColor)
+        .ignoresSafeArea(.all)
       
-      Text("Fim de Jogo!")
-        .font(.system(size: 34))
-        .fontWeight(.heavy)
-        .padding(.top, 50)
-      
-      Spacer(minLength: 30.0)
-      } //: HSTACK
-      
-      Spacer()
-      
-      GenericButtons(label: "Novo Jogo")
-        .frame(width: 233, height: 46)
-        .shadow(radius: 5)
-        .padding()
+      VStack {
         
-      GenericButtons(label: "Ranking", color: .gray)
-        .frame(width: 233, height: 46)
-        .shadow(radius: 5)
-
+        Text("Fim de Jogo!")
+          .font(.system(size: 34))
+          .foregroundColor(.white)
+          .fontWeight(.heavy)
+          .padding(.vertical, 30)
+        
+        VStack {
+          PlayerGameOver(name: "Alice", avatarAsset: "avatarRed", podiumPosition: 1, points: 20)
+            .padding(.bottom, 20)
+          
+          HStack(spacing: 60) {
+            PlayerGameOver(name: "João", avatarAsset: "avatarYellow", podiumPosition: 2, points: 16)
+            PlayerGameOver(name: "Zion", avatarAsset: "avatarTurquoise", podiumPosition: 3, points: 15)
+          }
+          
+        }
+        
+        Spacer()
+        
+        NeonButton(text: "Novo Jogo", image: .neonButtonYellow)
+          .frame(width: 233, height: 85)
+          .shadow(radius: 5)
+          .padding(20)
+      }
     } //: VSTACK
   }
+}
 
 // MARK: - PREVIEW
 struct GameOverView_Previews: PreviewProvider {
   static var previews: some View {
-    GameOverView()
+    let devices = ["iPhone SE (3rd generation)","iPhone 8", "iPhone 12", "iPhone 14", "iPhone 11 Pro Max"]
+    
+    ForEach(devices, id: \.self) { device in
+      GameOverView()
+        .previewDevice(PreviewDevice(rawValue: device))
+        .previewDisplayName(device)
+    }
   }
 }
