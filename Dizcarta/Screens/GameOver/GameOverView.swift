76ce3aca73b3bdaea0b8cbc3dc1ae244a8b73956
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-10-25 17:58:59 -0300

    feat(DIZ-197): Added the components and the logic to the Ranking Screen

--- Dizcarta/Screens/GameOver/GameOverView.swift
+++ Dizcarta/Screens/GameOver/GameOverView.swift
@@ -8,75 +8,95 @@
 import SwiftUI
 
 struct GameOverView: View {
-    // MARK: - VARIABLES
-    @EnvironmentObject var gameCore: GameCore
-    @State private var newGame = false
-    // MARK: - BODY
-    var body: some View {
-        NavigationStack {
-            GeometryReader { geometry in
-                ZStack {
-                    Color(.backgroundAppColor)
-                        .ignoresSafeArea(.all)
-                    
-                    VStack {
-                        
-                        Text("Fim de Jogo!")
-                            .font(.system(size: 34))
-                            .foregroundColor(.white)
-                            .fontWeight(.heavy)
-                            .padding(.vertical, 30)
-                        
-                        VStack {
-                            PlayerGameOver(name: gameCore.top3Players[0].wrappedName,
-                                           avatarAsset: gameCore.top3Players[0].wrappedAvatar, podiumPosition: 1,
-                                           points: gameCore.top3Players[0].wrappedPoints)
-                                .padding(.bottom, 20)
-                            
-                            HStack(spacing: 60) {
-                                PlayerGameOver(name: gameCore.top3Players[1].wrappedName,
-                                               avatarAsset: gameCore.top3Players[1].wrappedAvatar,
-                                               podiumPosition: 2,
-                                               points: gameCore.top3Players[1].wrappedPoints)
-                                PlayerGameOver(name: gameCore.top3Players[2].wrappedName,
-                                               avatarAsset: gameCore.top3Players[2].wrappedAvatar,
-                                               podiumPosition: 3,
-                                               points: gameCore.top3Players[2].wrappedPoints)
-                            }
-                            
-                        }
-                        
-                        Spacer()
-                        
-                        NeonButton(text: "Novo Jogo", image: .neonButtonYellow)
-                            .frame(width: geometry.size.width / 1.2, height: geometry.size.height / 7)
-                            .shadow(radius: 5)
-                            .padding(20)
-                            .onTapGesture {
-                                newGame = true
-                                gameCore.resetMatch()
-                            }
-                    }
+  // MARK: - VARIABLES
+  @EnvironmentObject var gameCore: GameCore
+  @State private var newGame = false
+  // MARK: - BODY
+  var body: some View {
+    NavigationStack {
+      GeometryReader { geometry in
+        ZStack {
+          Color(.backgroundAppColor)
+            .ignoresSafeArea(.all)
+          
+          ScrollView(.vertical, showsIndicators: true) {
+            VStack {
+              Text("Fim de Jogo!")
+                .font(.system(size: 34))
+                .foregroundColor(.white)
+                .fontWeight(.heavy)
+                .padding(.vertical, 30)
+              
+              VStack {
+                PlayerGameOver(name: gameCore.top3Players[0].wrappedName,
+                               avatarAsset: gameCore.top3Players[0].wrappedAvatar,
+                               points: gameCore.top3Players[0].wrappedPoints,
+                               podiumPosition: 1)
+                .padding(.bottom, 20)
+                
+                HStack(spacing: 60) {
+                  PlayerGameOver(name: gameCore.top3Players[1].wrappedName,
+                                 avatarAsset: gameCore.top3Players[1].wrappedAvatar,
+                                 points: gameCore.top3Players[1].wrappedPoints,
+                                 podiumPosition: 2)
+                  PlayerGameOver(name: gameCore.top3Players[2].wrappedName,
+                                 avatarAsset: gameCore.top3Players[2].wrappedAvatar,
+                                 points: gameCore.top3Players[2].wrappedPoints,
+                                 podiumPosition: 3)
                 }
-                .navigationBarBackButtonHidden(true)
-                .navigationDestination(isPresented: $newGame) {
-                    SetupMatchView()
+                
+                PlayerGameOver(name: gameCore.top3Players[3].wrappedName,
+                               avatarAsset: gameCore.top3Players[3].wrappedAvatar,
+                               points: gameCore.top3Players[3].wrappedPoints,
+                               podiumPosition: 4)
+                .padding()
+                
+                PlayerGameOver(name: gameCore.top3Players[4].wrappedName,
+                               avatarAsset: gameCore.top3Players[4].wrappedAvatar,
+                               points: gameCore.top3Players[4].wrappedPoints,
+                               podiumPosition: 5)
+                .padding()
+                
+                PlayerGameOver(name: gameCore.top3Players[5].wrappedName,
+                               avatarAsset: gameCore.top3Players[5].wrappedAvatar,
+                               points: gameCore.top3Players[5].wrappedPoints,
+                               podiumPosition: 6)
+                .padding()
+                
+              }
+              
+              Spacer()
+              
+              NeonButton(text: "Novo Jogo", image: .neonButtonYellow)
+                .frame(width: geometry.size.width / 1.2, height: geometry.size.height / 7)
+                .shadow(radius: 5)
+                .padding(20)
+                .onTapGesture {
+                  newGame = true
+                  gameCore.resetMatch()
+                }
             }
-            }
+          }
+          .navigationBarBackButtonHidden(true)
+          .navigationDestination(isPresented: $newGame) {
+            SetupMatchView()
+          }
         }
-        //: VSTACK
+      }
     }
+    //: VSTACK
+  }
 }
 
 // MARK: - PREVIEW
-struct GameOverView_Previews: PreviewProvider {
-    static var previews: some View {
-        let devices = ["iPhone SE (3rd generation)","iPhone 8", "iPhone 12", "iPhone 14", "iPhone 11 Pro Max"]
-        
-        ForEach(devices, id: \.self) { device in
-            GameOverView()
-                .previewDevice(PreviewDevice(rawValue: device))
-                .previewDisplayName(device)
-        }
-    }
-}
+// struct GameOverView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    let devices = ["iPhone SE (3rd generation)","iPhone 8", "iPhone 12", "iPhone 14", "iPhone 11 Pro Max"]
+//
+//    ForEach(devices, id: \.self) { device in
+//      GameOverView()
+//        .previewDevice(PreviewDevice(rawValue: device))
+//        .previewDisplayName(device)
+//    }
+//  }
+// }
--- Dizcarta/Screens/GameOver/Shared/PlayerGameOver.swift
+++ Dizcarta/Screens/GameOver/Shared/PlayerGameOver.swift
@@ -11,18 +11,25 @@ struct PlayerGameOver: View {
   // MARK: - VARIABLES
   @State var name: String
   @State var avatarAsset: String
-  @State var podiumPosition: Int
   @State var points: Int
+  let podiumPosition: Int
+  private let podiumAssets = [
+    1 : Color(.yellow),
+    2 : Color(.gray),
+    3 : Color(.brown),
+    4 : Color(.green)
+  ]
   
   // MARK: - BODY
   var body: some View {
-    VStack {
-      ZStack(alignment: .bottomLeading) {
-        Image(avatarAsset)
-          .resizable()
-          .frame(width: 150, height: 150)
-          .padding(-30)
-        
+    if podiumPosition != nil  && podiumPosition < 4 {
+      VStack {
+        ZStack(alignment: .bottomLeading) {
+          Image(avatarAsset)
+            .resizable()
+            .frame(width: 150, height: 150)
+            .padding(-30)
+          
           Text("\(podiumPosition)")
             .font(.system(size: 22))
             .fontWeight(.bold)
@@ -30,34 +37,67 @@ struct PlayerGameOver: View {
             .background(
               Circle()
                 .frame(width: 35, height: 35)
-                .foregroundColor(.white)
+                .foregroundColor(podiumAssets[podiumPosition])
                 .opacity(0.9)
             )
-      } //: ZSTACK
-      
-      Text(name)
-        .font(.system(size: 24))
-        .foregroundColor(.white)
-        .padding(.vertical, 2)
-      
-      Text("\(points) pontos")
-        .font(.system(size: 15))
-        .foregroundColor(.black)
-        .fontWeight(.semibold)
-        .padding(.horizontal, 25)
-        .padding(.vertical, 4)
-        .background {
-          RoundedRectangle(cornerRadius: 20)
-                .foregroundColor(.white)
-                .opacity(0.7)
+        } //: ZSTACK
+        
+        Text(name)
+          .font(.system(size: 24))
+          .foregroundColor(.white)
+          .padding(.vertical, 2)
+        
+        Text("\(points) pontos")
+          .font(.system(size: 15))
+          .foregroundColor(.white)
+          .fontWeight(.semibold)
+          .padding(.horizontal, 25)
+          .padding(.vertical, 4)
+      } //: VSTACK
+    } else {
+      HStack {
+        Text("\(podiumPosition)")
+          .font(.system(size: 22))
+          .fontWeight(.bold)
+          .foregroundColor(.black)
+          .background(
+            Circle()
+              .frame(width: 35, height: 35)
+              .foregroundColor(podiumAssets[4])
+              .opacity(0.9)
+          )
+          .padding(.horizontal, 40)
+                
+        Image(avatarAsset)
+          .resizable()
+          .frame(width: 96, height: 96)
+          .padding(-20)
+        
+        VStack(alignment: .leading) {
+          Text(name)
+            .font(.system(size: 18))
+            .foregroundColor(.white)
+            .padding(.vertical, 2)
+          
+          Text("\(points) pontos")
+            .font(.system(size: 14))
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+            .padding(.vertical, 4)
+          
         }
-    } //: VSTACK
+        .padding(.horizontal, 20)
+        
+        Spacer()
+      }
+    }
   }
 }
 
 // MARK: - PREVIEW
 struct PlayerGameOver_Previews: PreviewProvider {
   static var previews: some View {
-      PlayerGameOver(name: "Alice", avatarAsset: "avatarRed", podiumPosition: 1, points: 20)
+    PlayerGameOver(name: "Alice", avatarAsset: "avatarRed", points: 20, podiumPosition: 4)
+      .preferredColorScheme(.dark)
   }
 }
