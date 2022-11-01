f08bd69f54e332c414ec197fda4b6519bc9f662c
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-11-01 14:36:13 -0300

    Fixed some UI issues and adjusted the screen design

--- Dizcarta/Core/DesignSystems/Player/PlayerSelectedView.swift
+++ Dizcarta/Core/DesignSystems/Player/PlayerSelectedView.swift
@@ -8,84 +8,79 @@
 import SwiftUI
 
 struct PlayerSelectedView: View {
-    // MARK: - PROPERTIES
-    @State var isEditing: Bool = false
-    @FocusState var nameIsFocused: Bool
-    @ObservedObject var player: Player
-    @State private var backupName: String = ""
-    var saveAction: () -> Void
-    var deleteAction: () -> Void
-    
-    // MARK: - BODY
-    var body: some View {
-        GeometryReader { _ in
-            HStack {
-                if isEditing {
-                    GenericFunctions.checkIfImageExist(name: "removePlayer")
-                        .frame(width: 5, height: 5)
-                        .padding(.leading)
-                        .onTapGesture {
-                            deleteAction()
-                            self.isEditing.toggle()
-                        }
-                }
-                GenericFunctions.checkIfImageExist(name: player.wrappedAvatar)
-                    .resizable()
-                    .frame(width: 112, height: 112)
-                    .padding(.trailing, -15)
-                    .opacity(1.0)
-                
-                if isEditing {
-                    TextField("", text: $player.wrappedName)
-                        .foregroundColor(.white)
-                        .font(Font(name: .primaryFont, size: 20))
-                        .textInputAutocapitalization(.words)
-                        .autocorrectionDisabled(true)
-                        .focused($nameIsFocused)
-                        .modifier(TextFieldClearButton(text: $player.wrappedName))
-                        .padding(.horizontal, 18)
-                        .padding(.vertical, 8)
-                        .background(
-                            HStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color(.textFieldBackgroundColor))
-                            }
-                        )
-                    
-                } else {
-                    TextField("", text: $player.wrappedName)
-                        .foregroundColor(.white)
-                        .font(Font.custom("DINAlternate-Bold", size: 20))
-                        .disabled(!isEditing)
-                }
-                
-                Button {
-                    self.isEditing.toggle()
-                } label: {
-                    if isEditing {
-                        GenericFunctions.checkIfImageExist(name: "checkmark")
-                            .frame(width: 22, height: 18)
-                            .padding()
-                            .onTapGesture {
-                              if player.wrappedName.trimmingCharacters(in: .whitespaces).isEmpty {
-                                    player.wrappedName = backupName
-                                }
-                                saveAction()
-                                backupName = player.wrappedName
-                                self.isEditing.toggle()
-                            }
-                    } else {
-                        GenericFunctions.checkIfImageExist(name: "ChangeNamePencil")
-                            .resizable()
-                            .frame(width: 70, height: 70)
-                    }
-                }
-            }
-            .onAppear {
-                backupName = player.wrappedName
-            }
+  // MARK: - PROPERTIES
+  @State var isEditing: Bool = false
+  @FocusState var nameIsFocused: Bool
+  @ObservedObject var player: Player
+  @State private var backupName: String = ""
+  var saveAction: () -> Void
+  var deleteAction: () -> Void
+  
+  // MARK: - BODY
+  var body: some View {
+    GeometryReader { geometry in
+      HStack {
+        GenericFunctions.checkIfImageExist(name: player.wrappedAvatar)
+          .resizable()
+          .frame(width: 95, height: 95)
+          .padding(.leading, 10)
+          .padding(.trailing, -15)
+          .opacity(1.0)
+        
+        if isEditing {
+          TextField("", text: $player.wrappedName)
+            .foregroundColor(.white)
+            .font(Font(name: .primaryFont, size: 20))
+            .textInputAutocapitalization(.words)
+            .autocorrectionDisabled(true)
+            .focused($nameIsFocused)
+            .modifier(TextFieldClearButton(text: $player.wrappedName))
+            .padding(.vertical, 8)
+          
+        } else {
+          TextField("", text: $player.wrappedName)
+            .foregroundColor(.white)
+            .font(Font.custom("DINAlternate-Bold", size: 20))
+            .disabled(!isEditing)
         }
+        
+        Button {
+          self.isEditing.toggle()
+        } label: {
+          if isEditing {
+            GenericFunctions.checkIfImageExist(name: "checkmark")
+              .frame(width: 22, height: 18)
+              .padding(20)
+              .onTapGesture {
+                if player.wrappedName.trimmingCharacters(in: .whitespaces).isEmpty {
+                  player.wrappedName = backupName
+                }
+                saveAction()
+                backupName = player.wrappedName
+                self.isEditing.toggle()
+              }
+          } else {
+            Image(systemName: "pencil")
+              .resizable()
+              .frame(width: 16, height: 16)
+              .padding(22)
+              .foregroundColor(Color(.textPlayersCount))
+          }
+        }
+      }
+      .background(
+        HStack {
+          RoundedRectangle(cornerRadius: 5)
+            .foregroundColor(Color(.playerListBackgroundColor))
+            .frame(width: geometry.size.width - 25, height: 40)
+        }
+      )
+      .onAppear {
+        backupName = player.wrappedName
+      }
+      
     }
+  }
 }
 
 // MARK: - PREVIEW
--- Dizcarta/Core/DesignSystems/Views/TextField/TextFieldClearButton.swift
+++ Dizcarta/Core/DesignSystems/Views/TextField/TextFieldClearButton.swift
@@ -8,21 +8,19 @@
 import SwiftUI
 
 struct TextFieldClearButton: ViewModifier {
-    @Binding var text: String
-    
-    func body(content: Content) -> some View {
-        HStack {
-            content
-            
-            if !text.isEmpty {
-                Button(
-                    action: { self.text = "" },
-                    label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .foregroundColor(Color(UIColor.white))
-                    }
-                )
-            }
-        }
+  @Binding var text: String
+  
+  func body(content: Content) -> some View {
+    HStack {
+      content
+      
+      if !text.isEmpty {
+        Button(
+          action: { self.text = "" },
+          label: {
+          }
+        )
+      }
     }
+  }
 }
