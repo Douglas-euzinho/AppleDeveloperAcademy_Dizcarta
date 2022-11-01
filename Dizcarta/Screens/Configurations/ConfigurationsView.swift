367888405ce1e78c23df2d35c63993f884bc1fb8
Douglas Figueirôa <62218512+Douglas-euzinho@users.noreply.github.com>
2022-11-01 15:50:37 -0300

    refactor(DIZ-221): refactor configuration view

--- Dizcarta/Screens/Configurations/ConfigurationsView.swift
+++ Dizcarta/Screens/Configurations/ConfigurationsView.swift
@@ -8,63 +8,86 @@
 import SwiftUI
 
 struct ConfigurationsView: View {
-  // MARK: - VARIABLES
-  @Environment(\.presentationMode) var presentation
-  @State var isMessageOn = false
-  @State var messageToShow: String = ""
-  @StateObject var observed = Observed()
-  
-  // MARK: - BODY
-  var body: some View {
-    VStack {
-      HStack {
-        // TODO: - FIX THE FAST ACTIVATE AND DEACTIVATE THE BUTTONS LOGIC
-        ForEach(0..<2) { data in
-          ConfigButton(data: observed.buttonsData[data]) { message in
-            self.isMessageOn = true
-            self.messageToShow = message
-          }
+    // MARK: - VARIABLES
+    @Environment(\.presentationMode) var presentation
+    @StateObject var observed = Observed()
+    @State var isSpeakerOn = false
+    @State var isHapticsOn = false
+    @State var isColorBlindnessOn = false
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color(.backgroundAppColor)
+                .ignoresSafeArea(.all)
+            VStack(alignment: .leading) {
+                HStack(spacing: 20) {
+                    GenericFunctions.checkIfImageExist(name: "narrador")
+                    Toggle("Narrador", isOn: $isSpeakerOn)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                HStack(spacing: 20) {
+                    GenericFunctions.checkIfImageExist(name: "haptics")
+                    Toggle("Haptics", isOn: $isHapticsOn)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                HStack(spacing: 20) {
+                    GenericFunctions.checkIfImageExist(name: "color_blindness")
+                    Toggle("Color Blindness", isOn: $isColorBlindnessOn)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        GenericFunctions.checkIfImageExist(name: "regras")
+                        Text("Regras")
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                    }
+                }
+                .padding()
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        GenericFunctions.checkIfImageExist(name: "suporte")
+                        Text("Ajuda e Suporte")
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                    }
+                }
+                .padding()
+                
+            }
         }
-      }
-      .padding(.top, 50)
-      
-      HStack {
-        ForEach(2..<4) { data in
-          ConfigButton(data: observed.buttonsData[data]) { message in
-            self.isMessageOn = true
-            self.messageToShow = message
-          }
+        .navigationTitle("Configurações")
+        .navigationBarTitleDisplayMode(.large)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+                                HStack {
+            Image(systemName: "chevron.left")
+            Text("Voltar")
+                .fontWeight(.medium)
         }
-      }
-      .padding(.bottom, 35)
-      
-      ZStack {
-        if isMessageOn {
-          FeedbackButtonPress(hideMessage: $isMessageOn, selected: messageToShow)
-        }
-      }
-      Spacer()
+            .foregroundColor(.black)
+            .onTapGesture {
+                self.presentation.wrappedValue.dismiss()
+            }
+        )
     }
-    .navigationTitle("Configurações")
-    .navigationBarTitleDisplayMode(.large)
-    .navigationBarBackButtonHidden(true)
-    .navigationBarItems(leading:
-                          HStack {
-      Image(systemName: "chevron.left")
-      Text("Voltar")
-        .fontWeight(.medium)
-    }
-      .foregroundColor(.black)
-      .onTapGesture {
-        self.presentation.wrappedValue.dismiss()
-      }
-    )
-  }
 }
 
 // MARK: - PREVIEW
 struct ConfigurationsView_Previews: PreviewProvider {
-  static var previews: some View {
-    ConfigurationsView()
-  }
+    static var previews: some View {
+        ConfigurationsView()
+    }
 }
