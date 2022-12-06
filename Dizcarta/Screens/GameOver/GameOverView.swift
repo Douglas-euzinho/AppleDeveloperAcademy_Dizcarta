fa21879e4ca0ad32cfa973adf0c39de3d38d97ef
Douglas Figueirôa <62218512+Douglas-euzinho@users.noreply.github.com>
2022-12-06 15:00:50 -0300

    fix(DIZ-296): size text issue

--- Dizcarta/Screens/GameOver/GameOverView.swift
+++ Dizcarta/Screens/GameOver/GameOverView.swift
@@ -69,7 +69,7 @@ struct GameOverView: View {
                           HapticManager.send(style: .heavy)
                           router.newGame()
                       } label: {
-                          NeonButton(text: "Jogar Novamente", image: .redPrimaryButton, font: .dinCondensedBold, size: 30)
+                          NeonButton(text: "Jogar Novamente", image: .redPrimaryButton, font: .dinCondensedBold, size: 26)
                               .frame(width: geometry.size.width / 1.6, height: geometry.size.height / 15)
                               .shadow(radius: 5)
                               .padding(20)
